8580b3c1680602e3b99c3d2299e915091f161fb3
weiwenyu <2454150875@qq.com>
2023-09-12 21:30:43 +0800

    feat: 提交logo的修改

--- src/main/index.js
+++ src/main/index.js
@@ -1,20 +1,21 @@
-import { app, shell, BrowserWindow } from 'electron'
+import { app, shell, BrowserWindow, ipcMain, Tray, Menu } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
+import assets from './image/L.L.png?asset'
 
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 1200,
+    height: 800,
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
+    ...(process.platform === 'linux' ? { icon: assets } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false
-    }
+    },
+    icon: assets
   })
 
   mainWindow.webContents.toggleDevTools()
@@ -35,12 +36,25 @@ function createWindow() {
   } else {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
+
+  return mainWindow
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
+  const appTray = new Tray(assets)
+  // 监听点击托盘图标事件
+  // 点击托盘图标时显示/隐藏应用窗口
+  appTray.on('click', () => {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide()
+    } else {
+      mainWindow.show()
+    }
+  })
+
   // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
 
@@ -51,7 +65,29 @@ app.whenReady().then(() => {
     optimizer.watchWindowShortcuts(window)
   })
 
-  createWindow()
+  const mainWindow = createWindow()
+
+  const mainWindowCloseHandler = (event) => {
+    if (process.platform !== 'darwin') {
+      event.preventDefault()
+      mainWindow.hide()
+    }
+  }
+
+  mainWindow.on('close', mainWindowCloseHandler)
+
+  // 创建托盘菜单
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: '退出',
+      click: () => {
+        // 在退出之前解除主窗口的'close'事件监听
+        mainWindow.removeListener('close', mainWindowCloseHandler)
+        app.quit()
+      }
+    }
+  ])
+  appTray.setContextMenu(contextMenu)
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
@@ -69,5 +105,8 @@ app.on('window-all-closed', () => {
   }
 })
 
+app.on('will-quit', () => {
+  quit()
+})
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
